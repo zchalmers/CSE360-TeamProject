d95f7723698c9d6ce2b2ddfe8fb0647d942b7ad1
Fate <kyle116cobra@gmail.com>
2025-02-04 12:32:55 -0700

    Update SetupAccountPage.java
    format/comments/whitespace

--- TeamProjectPhase1/src/application/SetupAccountPage.java
+++ TeamProjectPhase1/src/application/SetupAccountPage.java
@@ -12,110 +12,112 @@ import java.util.List;
 import databasePart1.*;
 
 /**
- * SetupAccountPage class handles the account setup process for new users.
- * Users provide their userName, password, and a valid invitation code to register.
+ * SetupAccountPage class handles the account setup process for new users. Users
+ * provide their userName, password, and a valid invitation code to register.
  */
 public class SetupAccountPage {
-	
-    private final DatabaseHelper databaseHelper;
-    // DatabaseHelper to handle database operations.
-    public SetupAccountPage(DatabaseHelper databaseHelper) {
-        this.databaseHelper = databaseHelper;
-    }
 
-    /**
-     * Displays the Setup Account page in the provided stage.
-     * @param primaryStage The primary stage where the scene will be displayed.
-     */
-    public void show(Stage primaryStage) {
-    	// Input fields for userName, password, and invitation code
-    	TextField userNameField = new TextField();
-        userNameField.setPromptText("Enter UserName");
-        userNameField.setMaxWidth(250);
+	private final DatabaseHelper databaseHelper;
 
-        TextField nameField = new TextField();
-        nameField.setPromptText("Enter Name");
-        nameField.setMaxWidth(250);
-        
-        TextField emailField = new TextField();
-        emailField.setPromptText("Enter Email");
-        emailField.setMaxWidth(250);
-        
-        PasswordField passwordField = new PasswordField();
-        passwordField.setPromptText("Enter Password");
-        passwordField.setMaxWidth(250);
-        
-        TextField inviteCodeField = new TextField();
-        inviteCodeField.setPromptText("Enter InvitationCode");
-        inviteCodeField.setMaxWidth(250);
-        
-        // Label to display error messages for invalid input or registration issues
-        Label errorLabel = new Label();
-        errorLabel.setStyle("-fx-text-fill: red; -fx-font-size: 12px;");
-        
+	// DatabaseHelper to handle database operations.
+	public SetupAccountPage(DatabaseHelper databaseHelper) {
+		this.databaseHelper = databaseHelper;
+	}
 
-        Button setupButton = new Button("Setup");
-        
-        setupButton.setOnAction(a -> {
-        	// Retrieve user input
-        	String userName = userNameField.getText();
-            String name = nameField.getText();
-            String email = emailField.getText();
-            String password = passwordField.getText();
-            String code = inviteCodeField.getText();
-            
-            String userNameValidate = UserNameRecognizer.checkForValidUserName(userName); // Using FSM to validate Username
-            String passwordValidate = PasswordEvaluator.evaluatePassword(password);       // Using FSM to validate Password
-            
-            if (!userNameValidate.isEmpty()) {  // if there is no error then username is valid
-            	userNameValidate = "Username Error:" + userNameValidate;   // make it more descriptive
-            	errorLabel.setText(userNameValidate);      // set the errorlabel for username error
-            	return;
-            }
-            
-            if (!passwordValidate.isEmpty()) {
-            	passwordValidate = "Password Error: \n" + passwordValidate;
-            	errorLabel.setText(passwordValidate);
-        		return;
-            }
-            
-            try {
-            	// Check if the user already exists
-            	if(!databaseHelper.doesUserExist(userName)) {
-            		
-            		// Validate the invitation code
-            		if(databaseHelper.validateInvitationCode(code)) {
-            			
-            			// Create a new user and register them in the database
-            			List<String> roles = new ArrayList<>();
-                    	roles.add("student");
-                    
-                    	User user= new User(userName, name, password, email, roles);
-                        databaseHelper.register(user);
-		                
-		             // Navigate to the Welcome Login Page
-		                new UserLoginPage(databaseHelper).show(primaryStage);
-            		}
-            		else {
-            			errorLabel.setText("Please enter a valid invitation code.");
-            		}
-            	}
-            	else {
-            		errorLabel.setText("This useruserName is taken!!.. Please use another to setup an account");
-            	}
-            	
-            } catch (SQLException e) {
-                System.err.println("Database error: " + e.getMessage());
-                e.printStackTrace();
-            }
-        });
+	/**
+	 * Displays the Setup Account page in the provided stage.
+	 * 
+	 * @param primaryStage The primary stage where the scene will be displayed.
+	 */
+	public void show(Stage primaryStage) {
 
-        VBox layout = new VBox(10);
-        layout.setStyle("-fx-padding: 20; -fx-alignment: center;");
-        layout.getChildren().addAll(userNameField, nameField, emailField, passwordField,inviteCodeField, setupButton, errorLabel);
+		// Input fields for userName, password, and invitation code
+		TextField userNameField = new TextField();
+		userNameField.setPromptText("Enter UserName");
+		userNameField.setMaxWidth(250);
 
-        primaryStage.setScene(new Scene(layout, 940, 400));
-        primaryStage.setTitle("Account Setup");
-        primaryStage.show();
-    }
+		TextField nameField = new TextField();
+		nameField.setPromptText("Enter Name");
+		nameField.setMaxWidth(250);
+
+		TextField emailField = new TextField();
+		emailField.setPromptText("Enter Email");
+		emailField.setMaxWidth(250);
+
+		PasswordField passwordField = new PasswordField();
+		passwordField.setPromptText("Enter Password");
+		passwordField.setMaxWidth(250);
+
+		TextField inviteCodeField = new TextField();
+		inviteCodeField.setPromptText("Enter InvitationCode");
+		inviteCodeField.setMaxWidth(250);
+
+		// Label to display error messages for invalid input or registration issues
+		Label errorLabel = new Label();
+		errorLabel.setStyle("-fx-text-fill: red; -fx-font-size: 12px;");
+
+		Button setupButton = new Button("Setup");
+
+		setupButton.setOnAction(a -> {
+			// Retrieve user input
+			String userName = userNameField.getText();
+			String name = nameField.getText();
+			String email = emailField.getText();
+			String password = passwordField.getText();
+			String code = inviteCodeField.getText();
+
+			String userNameValidate = UserNameRecognizer.checkForValidUserName(userName); // Using FSM to validate
+																							// Username
+			String passwordValidate = PasswordEvaluator.evaluatePassword(password); // Using FSM to validate Password
+
+			if (!userNameValidate.isEmpty()) { // if there is no error then username is valid
+				userNameValidate = "Username Error:" + userNameValidate; // make it more descriptive
+				errorLabel.setText(userNameValidate); // set the errorlabel for username error
+				return;
+			}
+
+			if (!passwordValidate.isEmpty()) {
+				passwordValidate = "Password Error: \n" + passwordValidate;
+				errorLabel.setText(passwordValidate);
+				return;
+			}
+
+			try {
+				// Check if the user already exists
+				if (!databaseHelper.doesUserExist(userName)) {
+
+					// Validate the invitation code
+					if (databaseHelper.validateInvitationCode(code)) {
+
+						// Create a new user and register them in the database
+						List<String> roles = new ArrayList<>();
+						roles.add("student");
+
+						User user = new User(userName, name, password, email, roles);
+						databaseHelper.register(user);
+
+						// Navigate to the Welcome Login Page
+						new UserLoginPage(databaseHelper).show(primaryStage);
+					} else {
+						errorLabel.setText("Please enter a valid invitation code.");
+					}
+				} else {
+					errorLabel.setText("This useruserName is taken!!.. Please use another to setup an account");
+				}
+
+			} catch (SQLException e) {
+				System.err.println("Database error: " + e.getMessage());
+				e.printStackTrace();
+			}
+		});
+
+		VBox layout = new VBox(10);
+		layout.setStyle("-fx-padding: 20; -fx-alignment: center;");
+		layout.getChildren().addAll(userNameField, nameField, emailField, passwordField, inviteCodeField, setupButton,
+				errorLabel);
+
+		primaryStage.setScene(new Scene(layout, 940, 400));
+		primaryStage.setTitle("Account Setup");
+		primaryStage.show();
+	}
 }
