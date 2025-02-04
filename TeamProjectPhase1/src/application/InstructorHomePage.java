bcaa7c7cea4585808a44a464117e6fb7c3a0c9c5
Fate <kyle116cobra@gmail.com>
2025-02-04 12:27:12 -0700

    Update InstructorHomePage.java
    format/comments/whitespace

--- TeamProjectPhase1/src/application/InstructorHomePage.java
+++ TeamProjectPhase1/src/application/InstructorHomePage.java
@@ -8,42 +8,41 @@ import javafx.stage.Stage;
 import databasePart1.DatabaseHelper;
 
 /**
- * InstructorHomePage class represents the user interface for the instructor user.
- * This page displays a simple welcome message for the instructor.
+ * InstructorHomePage class represents the user interface for the instructor
+ * user. This page displays a simple welcome message for the instructor.
  */
-
 public class InstructorHomePage {
 	/**
-     * Displays the instructor page in the provided primary stage.
-     * @param primaryStage The primary stage where the scene will be displayed.
-     */
-	
+	 * Displays the instructor page in the provided primary stage.
+	 * 
+	 * @param primaryStage The primary stage where the scene will be displayed.
+	 */
 	private final DatabaseHelper databaseHelper;
 
-public InstructorHomePage(DatabaseHelper databaseHelper) {
-        this.databaseHelper = databaseHelper;
-    }
-	
-    public void show(Stage primaryStage) {
-    	VBox layout = new VBox(40);
-    	Button quitButton = new Button("Back to login");
-    	
-	    layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
-	    
-	    // label to display the welcome message for the instructor
-	    Label instructorLabel = new Label("Hello, Instructor!");
-	    
-	    instructorLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+	public InstructorHomePage(DatabaseHelper databaseHelper) {
+		this.databaseHelper = databaseHelper;
+	}
 
-	    quitButton.setOnAction(a -> {
-		    new UserLoginPage(databaseHelper).show(primaryStage);
-	    });
+	public void show(Stage primaryStage) {
+		VBox layout = new VBox(40);
+		layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
 
-	    layout.getChildren().addAll(instructorLabel, quitButton);
-	    Scene instructorScene = new Scene(layout, 940, 400);
+		// Label to display the welcome message for the instructor
+		Label instructorLabel = new Label("Hello, Instructor!");
+		instructorLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
 
-	    // Set the scene to primary stage
-	    primaryStage.setScene(instructorScene);
-	    primaryStage.setTitle("Instructor Page");
-    }
+		// Button to return to the login screen
+		Button quitButton = new Button("Back to login");
+
+		quitButton.setOnAction(a -> {
+			new UserLoginPage(databaseHelper).show(primaryStage);
+		});
+
+		layout.getChildren().addAll(instructorLabel, quitButton);
+		Scene instructorScene = new Scene(layout, 940, 400);
+
+		// Set the scene to primary stage
+		primaryStage.setScene(instructorScene);
+		primaryStage.setTitle("Instructor Page");
+	}
 }
