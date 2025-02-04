680617bab2f55db3a813704aa4e3dc19c4ec8582
Fate <kyle116cobra@gmail.com>
2025-02-04 12:34:32 -0700

    Update StaffHomePage.java
    format/comments/whitespace

--- TeamProjectPhase1/src/application/StaffHomePage.java
+++ TeamProjectPhase1/src/application/StaffHomePage.java
@@ -8,43 +8,41 @@ import javafx.stage.Stage;
 import databasePart1.DatabaseHelper;
 
 /**
- * StaffHomePage class represents the user interface for the staff user.
- * This page displays a simple welcome message for the staff.
+ * StaffHomePage class represents the user interface for the staff user. This
+ * page displays a simple welcome message for the staff.
  */
-
 public class StaffHomePage {
 	/**
-     * Displays the staff page in the provided primary stage.
-     * @param primaryStage The primary stage where the scene will be displayed.
-     */
-
+	 * Displays the staff page in the provided primary stage.
+	 * 
+	 * @param primaryStage The primary stage where the scene will be displayed.
+	 */
 	private final DatabaseHelper databaseHelper;
 
-public StaffHomePage(DatabaseHelper databaseHelper) {
-        this.databaseHelper = databaseHelper;
-    }
+	public StaffHomePage(DatabaseHelper databaseHelper) {
+		this.databaseHelper = databaseHelper;
+	}
 
-	
-    public void show(Stage primaryStage) {
-    	VBox layout = new VBox(40);
-    	Button quitButton = new Button("Back to login");
-    	
-	    layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
-	    
-	    // label to display the welcome message for the staff
-	    Label staffLabel = new Label("Hello, Staff!");
-	    
-	    staffLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+	public void show(Stage primaryStage) {
+		VBox layout = new VBox(40);
+		layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
 
-	    quitButton.setOnAction(a -> {
-		    new UserLoginPage(databaseHelper).show(primaryStage);
-	    });
+		// Label to display the welcome message for the staff
+		Label staffLabel = new Label("Hello, Staff!");
+		staffLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
 
-	    layout.getChildren().addAll(staffLabel, quitButton);
-	    Scene staffScene = new Scene(layout, 940, 400);
+		// Button to return to the login screen
+		Button quitButton = new Button("Back to login");
 
-	    // Set the scene to primary stage
-	    primaryStage.setScene(staffScene);
-	    primaryStage.setTitle("Staff Page");
-    }
+		quitButton.setOnAction(a -> {
+			new UserLoginPage(databaseHelper).show(primaryStage);
+		});
+
+		layout.getChildren().addAll(staffLabel, quitButton);
+		Scene staffScene = new Scene(layout, 940, 400);
+
+		// Set the scene to primary stage
+		primaryStage.setScene(staffScene);
+		primaryStage.setTitle("Staff Page");
+	}
 }
