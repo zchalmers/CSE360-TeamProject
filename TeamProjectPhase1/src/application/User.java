cf8a25682472104b0d407fea71e9b0e029f65fb3
Fate <kyle116cobra@gmail.com>
2025-02-04 12:39:44 -0700

    Update User.java
    format/comments/whitespace

--- TeamProjectPhase1/src/application/User.java
+++ TeamProjectPhase1/src/application/User.java
@@ -3,50 +3,68 @@ package application;
 import java.util.List;
 
 /**
- * The User class represents a user entity in the system.
- * It contains the user's details such as userName, password, and role.
+ * The User class represents a user entity in the system. It contains the user's
+ * details such as userName, password, and role.
  */
 public class User {
-    private String userName;
-    private String name;
-    private String password;
-    private String email;
-    private String currentRole;
-    private List<String> roles;
-    
-    // Constructor to initialize a new User object with userName, password, and role.
-    public User( String userName, String name, String password, String email, List<String> roles) {
-        this.userName = userName;
-        this.name = name;
-        this.password = password;
-        this.email = email;
-        this.roles = roles;
-    }
-    
-    public User(String username, String name, String password, String email, String currentRole, List<String> roles) {
-    	
-    }
+	private String userName;
+	private String name;
+	private String password;
+	private String email;
+	private String currentRole;
+	private List<String> roles;
 
-    // IF YOU ARE CALLING THIS UPDATE ROLES IN DATABASE
-    public void addRole(String role) {
-    	roles.add(role);
-    }
+	// Constructor to initialize a new User object with userName, password, and
+	// role.
+	public User(String userName, String name, String password, String email, List<String> roles) {
+		this.userName = userName;
+		this.name = name;
+		this.password = password;
+		this.email = email;
+		this.roles = roles;
+	}
 
-    public String getUsername() { return this.userName; }
-    public String getPassword() { return this.password; }
-    public String getEmail() { return this.email;}
-    public String getName() { return this.name; }
-    public String getCurrentRole() { return this.currentRole; }
-    public List<String> getRoles() { return this.roles; }
-    
-    public void setPassword(String password) {
-    	this.password = password;
-    }
-    public void setCurrentRole(String role) {
-    	this.currentRole = role;
-    }
-    
-    public String toString() {
-    	return String.format("USER: \n Username: %s, Name: %s, Email: %s, Roles: %s", userName, name, email, roles);
-    }
+	public User(String username, String name, String password, String email, String currentRole, List<String> roles) {
+
+	}
+
+	public void addRole(String role) {
+		roles.add(role);
+	}
+
+	public String getUsername() {
+		return this.userName;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getCurrentRole() {
+		return this.currentRole;
+	}
+
+	public List<String> getRoles() {
+		return this.roles;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setCurrentRole(String role) {
+		this.currentRole = role;
+	}
+
+	public String toString() {
+		return String.format("USER: \n Username: %s, Name: %s, Email: %s, Roles: %s", userName, name, email, roles);
+	}
 }
