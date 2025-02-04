feacaea1f45f2da80b316f153c6bdc6805a4a304
Fate <kyle116cobra@gmail.com>
2025-02-04 12:42:17 -0700

    Update UserHomePage.java
    format/comments/whitespace

--- TeamProjectPhase1/src/application/UserHomePage.java
+++ TeamProjectPhase1/src/application/UserHomePage.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import javafx.scene.Scene;
 
 import javafx.scene.control.*;
@@ -8,40 +7,37 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import databasePart1.DatabaseHelper;
+
 /**
  * This page displays a simple welcome message for the user.
  */
-
 public class UserHomePage {
-
 	private User user;
-	
+
 	public UserHomePage(User user) {
 		this.user = user;
 	}
-    public void show(Stage primaryStage) {
-    	VBox layout = new VBox();
-	    layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
-	    
-	    // Label to display UserName and Role 
-	    Label userLabel = new Label("Hello, " + user.getUsername() + ". Your current role is : " + user.getCurrentRole());
-	    userLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
-	    
-	    // Logout button to redirect user back to UserLoginPage
-	    Button logoutButton = new Button("Logout");
-	    	logoutButton.setOnAction(event -> {
-	    		//Redirect to login
-	    		new UserLoginPage(new DatabaseHelper()).show(primaryStage); 
-	    	});
 
-	    layout.getChildren().addAll(userLabel,logoutButton); // Including logout button
-	    Scene userScene = new Scene(layout, 940, 400);
+	public void show(Stage primaryStage) {
+		VBox layout = new VBox();
+		layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
 
-	    // Set the scene to primary stage
-	    primaryStage.setScene(userScene);
-	    primaryStage.setTitle("User Page");
-    	
-    }
+		// Label to display UserName and Role
+		Label userLabel = new Label(
+				"Hello, " + user.getUsername() + ". Your current role is : " + user.getCurrentRole());
+		userLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+
+		// Button to return to the login screen
+		Button quitButton = new Button("Back to login");
+		quitButton.setOnAction(event -> {
+			new UserLoginPage(new DatabaseHelper()).show(primaryStage);
+		});
+
+		layout.getChildren().addAll(userLabel, quitButton); // Including logout button
+		Scene userScene = new Scene(layout, 940, 400);
+
+		// Set the scene to primary stage
+		primaryStage.setScene(userScene);
+		primaryStage.setTitle("User Page");
+	}
 }
-
-
