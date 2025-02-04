36d450c7b8fb6b046a04e275fb94f4253553e46f
Fate <kyle116cobra@gmail.com>
2025-02-04 12:28:27 -0700

    Update InvitationPage.java
    formate/comments/whitespace

--- TeamProjectPhase1/src/application/InvitationPage.java
+++ TeamProjectPhase1/src/application/InvitationPage.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import databasePart1.*;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,55 +8,55 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
- * InvitePage class represents the page where an admin can generate an invitation code.
- * The invitation code is displayed upon clicking a button.
+ * InvitePage class represents the page where an admin can generate an
+ * invitation code. The invitation code is displayed upon clicking a button.
  */
-
 public class InvitationPage {
-
 	/**
-     * Displays the Invite Page in the provided primary stage.
-     * 
-     * @param databaseHelper An instance of DatabaseHelper to handle database operations.
-     * @param primaryStage   The primary stage where the scene will be displayed.
-     */
-    public void show(DatabaseHelper databaseHelper,Stage primaryStage) {
-    	VBox layout = new VBox(10);
-	    layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
-	    
-	    // Label to display the title of the page
-	    Label userLabel = new Label("Invite ");
-	    userLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
-	    
-	    // Button to generate the invitation code
-	    Button showCodeButton = new Button("Generate Invitation Code");
-	    
-	    // Set showCodeButton as default so Enter can activate
-	    showCodeButton.setDefaultButton(true);
-	    
-	    // Button to return to login screen
-	    Button quitButton = new Button("Back to Login");
-	    	    
-	    // Label to display the generated invitation code
-	    Label inviteCodeLabel = new Label(""); ;
-        inviteCodeLabel.setStyle("-fx-font-size: 14px; -fx-font-style: italic;");
-        
-        showCodeButton.setOnAction(a -> {
-        	// Generate the invitation code using the databaseHelper and set it to the label
-            String invitationCode = databaseHelper.generateInvitationCode();
-            inviteCodeLabel.setText(invitationCode + " - Code will expire in 15 minutes.");
-        });
-	    
-       quitButton.setOnAction(a -> {
-    	   new UserLoginPage(databaseHelper).show(primaryStage);
-        });
+	 * Displays the Invite Page in the provided primary stage.
+	 * 
+	 * @param databaseHelper An instance of DatabaseHelper to handle database
+	 *                       operations.
+	 * @param primaryStage   The primary stage where the scene will be displayed.
+	 */
+	public void show(DatabaseHelper databaseHelper, Stage primaryStage) {
+		VBox layout = new VBox(10);
+		layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
 
-        layout.getChildren().addAll( userLabel, showCodeButton, inviteCodeLabel, quitButton);
-	    Scene inviteScene = new Scene(layout, 940, 400);
+		// Label to display the title of the page
+		Label userLabel = new Label("Invite ");
+		userLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
 
-	    // Set the scene to primary stage
-	    primaryStage.setScene(inviteScene);
-	    primaryStage.setTitle("Invite Page");
-    	
-    }
+		// Button to generate the invitation code
+		Button showCodeButton = new Button("Generate Invitation Code");
+
+		// Set showCodeButton as default so Enter can activate
+		showCodeButton.setDefaultButton(true);
+
+		// Button to return to login screen
+		Button quitButton = new Button("Back to Login");
+
+		// Label to display the generated invitation code
+		Label inviteCodeLabel = new Label("");
+		;
+		inviteCodeLabel.setStyle("-fx-font-size: 14px; -fx-font-style: italic;");
+
+		showCodeButton.setOnAction(a -> {
+			// Generate the invitation code using the databaseHelper and set it to the label
+			String invitationCode = databaseHelper.generateInvitationCode();
+			inviteCodeLabel.setText(invitationCode + " - Code will expire in 15 minutes.");
+		});
+
+		quitButton.setOnAction(a -> {
+			new UserLoginPage(databaseHelper).show(primaryStage);
+		});
+
+		layout.getChildren().addAll(userLabel, showCodeButton, inviteCodeLabel, quitButton);
+		Scene inviteScene = new Scene(layout, 940, 400);
+
+		// Set the scene to primary stage
+		primaryStage.setScene(inviteScene);
+		primaryStage.setTitle("Invite Page");
+
+	}
 }
