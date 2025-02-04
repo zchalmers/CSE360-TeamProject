87b788e0cde02dea71c95d7a81fecba7e0d0f1d4
Fate <kyle116cobra@gmail.com>
2025-02-04 12:55:23 -0700

    Update UserNameRecognizer.java
    edited error messages

--- TeamProjectPhase1/src/application/UserNameRecognizer.java
+++ TeamProjectPhase1/src/application/UserNameRecognizer.java
@@ -1,58 +1,68 @@
 package application;
 
-
 public class UserNameRecognizer {
 	/**
-	 * <p> Title: FSM-translated UserNameRecognizer. </p>
+	 * <p>
+	 * Title: FSM-translated UserNameRecognizer.
+	 * </p>
 	 * 
-	 * <p> Description: A demonstration of the mechanical translation of Finite State Machine 
-	 * diagram into an executable Java program using the UserName Recognizer. The code 
-	 * detailed design is based on a while loop with a select list</p>
+	 * <p>
+	 * Description: A demonstration of the mechanical translation of Finite State
+	 * Machine diagram into an executable Java program using the UserName
+	 * Recognizer. The code detailed design is based on a while loop with a select
+	 * list
+	 * </p>
 	 * 
-	 * <p> Copyright: Lynn Robert Carter © 2024 </p>
+	 * <p>
+	 * Copyright: Lynn Robert Carter © 2024
+	 * </p>
 	 * 
 	 * @author Lynn Robert Carter
 	 * 
-	 * @version 1.00		2024-09-13	Initial baseline derived from the Even Recognizer
-	 * @version 1.01		2024-09-17	Correction to address UNChar coding error, improper error
-	 * 									message, and improve internal documentation
-	 * 
+	 * @version 1.00 2024-09-13 Initial baseline derived from the Even Recognizer
+	 * @version 1.01 2024-09-17 Correction to address UNChar coding error, improper
+	 *          error message, and improve internal documentation
+	 * @version 1.02 2025-01-19 Adjustments to FSM to only accept alphabetic char in
+	 *          first step and to allow - and _ along with . between chars
 	 */
 
 	/**********************************************************************************************
 	 * 
-	 * Result attributes to be used for GUI applications where a detailed error message and a 
-	 * pointer to the character of the error will enhance the user experience.
+	 * Result attributes to be used for GUI applications where a detailed error
+	 * message and a pointer to the character of the error will enhance the user
+	 * experience.
 	 * 
 	 */
 
-	public static String userNameRecognizerErrorMessage = "";	// The error message text
-	public static String userNameRecognizerInput = "";			// The input being processed
-	public static int userNameRecognizerIndexofError = -1;		// The index of error location
-	private static int state = 0;						// The current state value
-	private static int nextState = 0;					// The next state value
-	private static boolean finalState = false;			// Is this state a final state?
-	private static String inputLine = "";				// The input line
-	private static char currentChar;					// The current character in the line
-	private static int currentCharNdx;					// The index of the current character
-	private static boolean running;						// The flag that specifies if the FSM is running
-	private static int userNameSize = 0;			// A numeric value may not exceed 16 characters
-		
+	public static String userNameRecognizerErrorMessage = ""; // The error message text
+	public static String userNameRecognizerInput = ""; // The input being processed
+	public static int userNameRecognizerIndexofError = -1; // The index of error location
+	private static int state = 0; // The current state value
+	private static int nextState = 0; // The next state value
+	private static boolean finalState = false; // Is this state a final state?
+	private static String inputLine = ""; // The input line
+	private static char currentChar; // The current character in the line
+	private static int currentCharNdx; // The index of the current character
+	private static boolean running; // The flag that specifies if the FSM is
+									// running
+	private static int userNameSize = 0; // A numeric value may not exceed 16 characters
+
 	// Private method to display debugging data
 	private static void displayDebuggingInfo() {
 		// Display the current state of the FSM as part of an execution trace
 		if (currentCharNdx >= inputLine.length())
 			// display the line with the current state numbers aligned
-			System.out.println(((state > 99) ? " " : (state > 9) ? "  " : "   ") + state + 
-					((finalState) ? "       F   " : "           ") + "None");
+			System.out.println(((state > 99) ? " " : (state > 9) ? "  " : "   ") + state
+					+ ((finalState) ? "       F   " : "           ") + "None");
 		else
-			System.out.println(((state > 99) ? " " : (state > 9) ? "  " : "   ") + state + 
-				((finalState) ? "       F   " : "           ") + "  " + currentChar + " " + 
-				((nextState > 99) ? "" : (nextState > 9) || (nextState == -1) ? "   " : "    ") + 
-				nextState + "     " + userNameSize);
+			System.out.println(((state > 99) ? " " : (state > 9) ? "  " : "   ") + state
+					+ ((finalState) ? "       F   " : "           ") + "  " + currentChar + " "
+					+ ((nextState > 99) ? "" : (nextState > 9) || (nextState == -1) ? "   " : "    ") + nextState
+					+ "     " + userNameSize);
 	}
-	
-	// Private method to move to the next character within the limits of the input line
+
+	// Private method to move to the next character within the limits of the input
+	// line
 	private static void moveToNextCharacter() {
 		currentCharNdx++;
 		if (currentCharNdx < inputLine.length())
@@ -64,193 +74,214 @@ public class UserNameRecognizer {
 	}
 
 	/**********
-	 * This method is a mechanical transformation of a Finite State Machine diagram into a Java
-	 * method.
+	 * This method is a mechanical transformation of a Finite State Machine diagram
+	 * into a Java method.
 	 * 
-	 * @param input		The input string for the Finite State Machine
-	 * @return			An output string that is empty if every things is okay or it is a String
-	 * 						with a helpful description of the error
+	 * @param input The input string for the Finite State Machine
+	 * @return An output string that is empty if every things is okay or it is a
+	 *         String with a helpful description of the error
 	 */
 	public static String checkForValidUserName(String input) {
 		// Check to ensure that there is input to process
-		if(input.length() <= 0) {
-			userNameRecognizerIndexofError = 0;	// Error at first character;
-			return "\n*** ERROR *** The UserName is empty";
+		if (input.length() <= 0) {
+			userNameRecognizerIndexofError = 0; // Error at first character;
+			return "\n*** ERROR *** The username is empty!!";
 		}
-		
+
 		// The local variables used to perform the Finite State Machine simulation
-		state = 0;							// This is the FSM state number
-		inputLine = input;					// Save the reference to the input line as a global
-		currentCharNdx = 0;					// The index of the current character
-		currentChar = input.charAt(0);		// The current character from above indexed position
-		
-		// The Finite State Machines continues until the end of the input is reached or at some 
-		// state the current character does not match any valid transition to a next state
+		state = 0; // This is the FSM state number
+		inputLine = input; // Save the reference to the input line as a global
+		currentCharNdx = 0; // The index of the current character
+		currentChar = input.charAt(0); // The current character from above indexed position
 
-		userNameRecognizerInput = input;	// Save a copy of the input
-		running = true;						// Start the loop
-		nextState = -1;						// There is no next state
+		// The Finite State Machines continues until the end of the input is reached or
+		// at some
+		// state the current character does not match any valid transition to a next
+		// state
+
+		userNameRecognizerInput = input; // Save a copy of the input
+		running = true; // Start the loop
+		nextState = -1; // There is no next state
 		System.out.println("\nCurrent Final Input  Next  Date\nState   State Char  State  Size");
-		
-		// This is the place where semantic actions for a transition to the initial state occur
-		
-		userNameSize = 0;					// Initialize the UserName size
 
-		// The Finite State Machines continues until the end of the input is reached or at some 
-		// state the current character does not match any valid transition to a next state
+		// This is the place where semantic actions for a transition to the initial
+		// state occur
+
+		userNameSize = 0; // Initialize the UserName size
+
+		// The Finite State Machines continues until the end of the input is reached or
+		// at some
+		// state the current character does not match any valid transition to a next
+		// state
 		while (running) {
-			// The switch statement takes the execution to the code for the current state, where
-			// that code sees whether or not the current character is valid to transition to a
+			// The switch statement takes the execution to the code for the current state,
+			// where
+			// that code sees whether or not the current character is valid to transition to
+			// a
 			// next state
 			switch (state) {
-			case 0: 
+			case 0:
 				// State 0 has 1 valid transition that is addressed by an if statement.
-				
-				// The current character is checked against A-Z, a-z, 0-9. If any are matched
+
+				// The current character is checked against A-Z, a-z. If any are matched
 				// the FSM goes to state 1
-				
+
 				// A-Z, a-z -> State 1
-				if ((currentChar >= 'A' && currentChar <= 'Z' ) ||		// Check for A-Z
-						(currentChar >= 'a' && currentChar <= 'z' )) {	// Check for a-z
+				if ((currentChar >= 'A' && currentChar <= 'Z') || // Check for A-Z
+						(currentChar >= 'a' && currentChar <= 'z')) { // Check for a-z
 					nextState = 1;
-					// Count the character 
+
+					// Count the character
 					userNameSize++;
-					
+
 					// This only occurs once, so there is no need to check for the size getting
 					// too large.
 				}
 				// If it is none of those characters, the FSM halts
-				else 
+				else
 					running = false;
-				
+
 				// The execution of this state is finished
 				break;
-			
-			case 1: 
-				// State 1 has two valid transitions, 
-				//	1: a A-Z, a-z, 0-9 that transitions back to state 1
-				//  2: a A-Z, a-z, 0-9, or . or - or _ that transitions to state 2
-					
-				
-				// A-Z, a-z, 0-9 -> State 1
-				// - , _ or . -> State 2
-				if ((currentChar >= 'A' && currentChar <= 'Z') || (currentChar >= 'a' && currentChar <= 'z') || 
-				        (currentChar >= '0' && currentChar <= '9')) {
-				        nextState = 1;
-				        userNameSize++;
-				} 
-				else if (currentChar == '-' || currentChar == '_' || currentChar == '.') {
-				        nextState = 2;
-				        userNameSize++;		
-				}
 
+			case 1:
+				// State 1 has two valid transitions,
+				// 1: a A-Z, a-z, 0-9 that transitions back to state 1
+				// 2: a period, minus or underscore that transitions to state 2
+
+				// A-Z, a-z, 0-9 transition to State 1
+				if ((currentChar >= 'A' && currentChar <= 'Z') || // Check for A-Z
+						(currentChar >= 'a' && currentChar <= 'z') || // Check for a-z
+						(currentChar >= '0' && currentChar <= '9')) { // Check for 0-9
+					nextState = 1;
+
+					// Count the character
+					userNameSize++;
+				}
+				// period, minus or underscore transition to State 2
+				else if ((currentChar == '.') || // Check for period
+						(currentChar == '-') || // Check for minus
+						(currentChar == '_')) { // Check for underscore
+					nextState = 2;
+
+					// Count the character
+					userNameSize++;
+				}
 				// If it is none of those characters, the FSM halts
 				else
 					running = false;
-				
+
 				// The execution of this state is finished
 				// If the size is larger than 16, the loop must stop
 				if (userNameSize > 16)
 					running = false;
-				break;			
-				
-			case 2: 
-				// State 2 deals with a character after a period, underscore or minus sign in the name.
-				
+				break;
+
+			case 2:
+				// State 2 deals with a character after a period, minus or underscore in the
+				// name.
+
 				// A-Z, a-z, 0-9 -> State 1
-				if ((currentChar >= 'A' && currentChar <= 'Z') || (currentChar >= 'a' && currentChar <= 'z') ||
-				        (currentChar >= '0' && currentChar <= '9')) {
-				        nextState = 1;
-				        userNameSize++;
+				if ((currentChar >= 'A' && currentChar <= 'Z') || // Check for A-Z
+						(currentChar >= 'a' && currentChar <= 'z') || // Check for a-z
+						(currentChar >= '0' && currentChar <= '9')) { // Check for 0-9
+					nextState = 1;
+
+					// Count the character
+					userNameSize++;
 				}
 				// If it is none of those characters, the FSM halts
-				else 
+				else
 					running = false;
 
 				// The execution of this state is finished
 				// If the size is larger than 16, the loop must stop
 				if (userNameSize > 16)
 					running = false;
-				break;			
+
+				break;
 			}
-			
+
 			if (running) {
 				displayDebuggingInfo();
 				// When the processing of a state has finished, the FSM proceeds to the next
 				// character in the input and if there is one, it fetches that character and
-				// updates the currentChar.  If there is no next character the currentChar is
+				// updates the currentChar. If there is no next character the currentChar is
 				// set to a blank.
 				moveToNextCharacter();
 
 				// Move to the next state
 				state = nextState;
-				
-				// Is the new state a final state?  If so, signal this fact.
-				if (state == 1) finalState = true;
+
+				// Is the new state a final state? If so, signal this fact.
+				if (state == 1)
+					finalState = true;
 
 				// Ensure that one of the cases sets this to a valid value
 				nextState = -1;
 			}
 			// Should the FSM get here, the loop starts again
-	
+
 		}
 		displayDebuggingInfo();
-		
+
 		System.out.println("The loop has ended.");
-		
-		// When the FSM halts, we must determine if the situation is an error or not.  That depends
-		// of the current state of the FSM and whether or not the whole string has been consumed.
-		// This switch directs the execution to separate code for each of the FSM states and that
-		// makes it possible for this code to display a very specific error message to improve the
+
+		// When the FSM halts, we must determine if the situation is an error or not.
+		// That depends
+		// of the current state of the FSM and whether or not the whole string has been
+		// consumed.
+		// This switch directs the execution to separate code for each of the FSM states
+		// and that
+		// makes it possible for this code to display a very specific error message to
+		// improve the
 		// user experience.
-		userNameRecognizerIndexofError = currentCharNdx;	// Set index of a possible error;
+		userNameRecognizerIndexofError = currentCharNdx; // Set index of a possible error;
 		userNameRecognizerErrorMessage = "\n*** ERROR *** ";
-		
+
 		// The following code is a slight variation to support just console output.
 		switch (state) {
 		case 0:
 			// State 0 is not a final state, so we can return a very specific error message
-			userNameRecognizerErrorMessage += "A UserName must start with A-Z or a-z\n";
+			userNameRecognizerErrorMessage += "A UserName must start with A-Z or a-z.\n";
 			return userNameRecognizerErrorMessage;
 
 		case 1:
-			// State 1 is a final state.  Check to see if the UserName length is valid.  If so we
+			// State 1 is a final state. Check to see if the UserName length is valid. If so
+			// we
 			// we must ensure the whole string has been consumed.
 
 			if (userNameSize < 4) {
 				// UserName is too small
 				userNameRecognizerErrorMessage += "A UserName must have at least 4 characters.\n";
 				return userNameRecognizerErrorMessage;
-			}
-			else if (userNameSize > 16) {
+			} else if (userNameSize > 16) {
 				// UserName is too long
-				userNameRecognizerErrorMessage += 
-					"A UserName must have no more than 16 character.\n";
+				userNameRecognizerErrorMessage += "A UserName must have no more than 16 characters.\n";
 				return userNameRecognizerErrorMessage;
-			}
-			else if (currentCharNdx < input.length()) {
+			} else if (currentCharNdx < input.length()) {
 				// There are characters remaining in the input, so the input is not valid
-				userNameRecognizerErrorMessage += 
-					"A UserName character may only contain the characters A-Z, a-z, 0-9.\n";
+				userNameRecognizerErrorMessage += "A UserName character may only contain the characters A-Z, a-z, 0-9 or period, "
+						+ "minus, or underscore.\n";
 				return userNameRecognizerErrorMessage;
-			}
-			else {
-					// UserName is valid
-					userNameRecognizerIndexofError = -1;
-					userNameRecognizerErrorMessage = "";
-					return userNameRecognizerErrorMessage;
+			} else {
+				// UserName is valid
+				userNameRecognizerIndexofError = -1;
+				userNameRecognizerErrorMessage = "";
+				return userNameRecognizerErrorMessage;
 			}
 
 		case 2:
 			// State 2 is not a final state, so we can return a very specific error message
-			userNameRecognizerErrorMessage +=
-				"A UserName character after a period, underscore or minus sign must be A-Z, a-z, 0-9.\n";
+			userNameRecognizerErrorMessage += "A UserName character after a period, minus or underscore must be A-Z, a-z, 0-9.\n";
 			return userNameRecognizerErrorMessage;
-			
+
 		default:
-			// This is for the case where we have a state that is outside of the valid range.
+			// This is for the case where we have a state that is outside of the valid
+			// range.
 			// This should not happen
-			return "";
+			userNameRecognizerErrorMessage += "State outside of valid range.";
+			return userNameRecognizerErrorMessage;
 		}
 	}
 }
